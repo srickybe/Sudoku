64f4f1e53d3bd5ed721b0a470b4b775c1b70c00f
Ricky <Ricky@DESKTOP-8F3SEUF>
2021-03-07 19:17:55 +0100

    Implementation of the Coordinates class

---
+++ src/sudoku/Coordinates.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sudoku;
+
+/**
+ *
+ * @author Ricky
+ */
+public class Coordinates {
+    private final int row;
+    private final int column;
+
+    public Coordinates(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    @Override
+    public String toString() {
+        return "Coordinates{" + "row=" + row + ", column=" + column + '}';
+    }
+}
