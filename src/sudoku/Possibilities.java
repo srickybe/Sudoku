1ce52659c57018b1af62195ab32df0b26f17352a
Ricky <Ricky@DESKTOP-8F3SEUF>
2021-03-07 19:19:38 +0100

    Implementation of the class Possibilities where all possible values for an element are stored

---
+++ src/sudoku/Possibilities.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sudoku;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Ricky
+ */
+public class Possibilities {
+    private final ArrayList<Integer> possible;
+
+    public Possibilities() {
+        possible = new ArrayList<>();
+    }
+    
+    public void add(int poss) {
+        possible.add(poss);
+    }
+}
